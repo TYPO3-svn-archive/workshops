269a7a47b6ed73a8e31f9c57a532048d2b2a0c44
padberg <padberg@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-10-10 20:55:43 +0000

    CALENDAR code added for displaying workshops overview in calendar style
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/workshops/trunk@6894 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cms']['db_layout']['addTables']['tx_workshops'][0] = array(
+    'fList' => 'title;datetime_begin;city',
+    'icon' => TRUE
+);
+
+?>
